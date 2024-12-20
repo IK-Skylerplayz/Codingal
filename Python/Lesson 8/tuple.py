9b0829fc36045d3daada00f5b25b109725ffa95c
IK_Skyler <ik.skyler404@gmail.com>
2024-12-20 20:55:13 +0300

    ss

---
+++ Python/Lesson 8/tuple.py
@@ -0,0 +1,23 @@
+my_tuple =()
+print(my_tuple)
+
+my_tuple = (1, 2, 3)
+print(my_tuple)
+
+my_tuple = ("mouse", [8, 4, 6], (1, 2, 3))
+print(my_tuple)
+
+my_tuple = ('E', 'L', 'K', 'A', 'N')
+print(my_tuple[2])
+print(my_tuple[4])
+
+n_tuple = ("mouse", [8, 4, 6], (1,2,3))
+
+print(n_tuple[0][3])
+print(n_tuple[1][1])
+
+print("Sliced :",my_tuple[1:4])
+
+for letter in (my_tuple):
+    print("Hello", letter)
+
