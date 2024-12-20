56237dfab4d4e7227d3d262db599fdf96b357636
IK_Skyler <ik.skyler404@gmail.com>
2024-12-20 20:31:00 +0300

    ss

---
+++ Python/Lesson 8/tupple_list.py
@@ -0,0 +1,8 @@
+def convert_tuple_to_list(input_tuple):
+    return list(input_tuple)
+
+sample_tuple = (1, 2, 3, 4, 5)
+converted_list = convert_tuple_to_list(sample_tuple)
+
+print("Original Tuple:", sample_tuple)
+print("Converted List:", converted_list)
